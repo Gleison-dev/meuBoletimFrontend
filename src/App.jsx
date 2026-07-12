3f412d79fe371e9376ab92c8ee58c26b7ac4b821
Gleison <gleisonrgomes@gmail.com>
2026-07-12 14:09:02 -0300

    feat: Header imported on app jjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/home";
+import Header from "./components/Header/header";
 
 export default function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
