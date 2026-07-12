7749d5dd9312a281334d8d2cf015669b7bc8b173
Gleison <gleisonrgomes@gmail.com>
2026-07-12 20:10:14 -0300

    feat: Cards of qualities created.

--- src/components/Qualities/qualities.jsx
+++ src/components/Qualities/qualities.jsx
@@ -1,7 +1,15 @@
-export default function Qualities() {
+export default function Qualities({img, alt, title, text}) {
   return (
     <>
-      <section></section>
+      <section>
+        <div>
+          <img src={img} alt={alt} />
+        </div>
+        <div>
+          <h1>{title}</h1>
+          <p>{text}</p>
+        </div>
+      </section>
     </>
   );
 }
