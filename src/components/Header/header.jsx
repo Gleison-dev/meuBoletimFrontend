5512c2c306fc5fe9e39b8afc25e50b8e3c0446cf
Gleison <gleisonrgomes@gmail.com>
2026-07-12 14:39:54 -0300

    feat: Header created.

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <>
+      <section className="h-25 flex justify-between items-center shadow px-10 bg-azul-claro">
+        <div>
+          <h1 className="text-lg">
+            <span className="font-semibold">Meu</span> <br /> <span className="text-azul">Boletim</span>
+          </h1>
+        </div>
+        <div>
+          <button className="w-30 h-10 rounded-lg text-lg cursor-pointer bg-azul text-branco">Entrar</button>
+        </div>
+      </section>
+    </>
+  );
+}
