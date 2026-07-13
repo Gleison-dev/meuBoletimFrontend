8e12cebb3d65bee2f79eb84df9927b8ecfcc4876
Gleison <gleisonrgomes@gmail.com>
2026-07-13 20:17:34 -0300

    feat: Login archive created.

---
+++ src/pages/Login/login.jsx
@@ -0,0 +1,12 @@
+export default function Login() {
+  return (
+    <>
+      <section>
+        <div>
+            <h1>LOGIN</h1>
+            <p>Faça seu login abaixo.</p>
+        </div>
+      </section>
+    </>
+  );
+}
