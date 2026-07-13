61726df3a3d9c4673917610ae05de55f3a200c07
Gleison <gleisonrgomes@gmail.com>
2026-07-13 20:19:42 -0300

    feat: NavLink inputed on button to login page.

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,14 +1,21 @@
+import { NavLink } from "react-router-dom";
+
 export default function Header() {
   return (
     <>
       <section className="h-25 flex justify-between items-center shadow px-10 bg-azul-claro">
         <div>
           <h1 className="text-lg">
-            <span className="font-semibold">Meu</span> <br /> <span className="text-azul">Boletim</span>
+            <span className="font-semibold">Meu</span> <br />{" "}
+            <span className="text-azul">Boletim</span>
           </h1>
         </div>
         <div>
-          <button className="w-30 h-10 rounded-lg text-lg cursor-pointer bg-azul text-branco">Entrar</button>
+          <NavLink to="/login">
+            <button className="w-30 h-10 rounded-lg text-lg cursor-pointer bg-azul text-branco">
+              Entrar
+            </button>
+          </NavLink>
         </div>
       </section>
     </>
