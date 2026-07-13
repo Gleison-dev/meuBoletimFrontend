2969d82ed1559dca7f1fe32ba13be4feda844830
Gleison <gleisonrgomes@gmail.com>
2026-07-13 20:18:28 -0300

    feat: Link to login created on app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/home";
 import Header from "./components/Header/header";
+import Login from "./pages/Login/login";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </>
   );
