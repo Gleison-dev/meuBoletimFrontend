b7ff3ea8e4143b681c1bc3cd61a7ed223ab9c3dd
Gleison <gleisonrgomes@gmail.com>
2026-07-14 14:10:47 -0300

    feat: Login of teacher page created

---
+++ src/pages/LoginTeacher/loginTeacher.jsx
@@ -0,0 +1,50 @@
+import { NavLink } from "react-router-dom";
+import Input from "../../components/Input/input";
+import "./styles.css";
+
+export default function LoginTeacher() {
+  return (
+    <>
+      <section className="flex justify-center mt-10">
+        <div className="pl-20 w-96 rounded-xl main-login flex flex-col justify-center bg-azul-claro">
+          <div>
+            <h1 className="text-xl">
+              <strong>LOGIN</strong>
+            </h1>
+            <p>Faça seu login de professor(a) abaixo.</p>
+          </div>
+          <form className="flex flex-col gap-3 mt-5">
+            <div>
+              <label>
+                <strong>E-mail:</strong>
+              </label>{" "}
+              <br />
+              <Input type="email" placeholder="Insira o seu e-mail" />
+            </div>
+            <div>
+              <label>
+                <strong>Senha:</strong>
+              </label>{" "}
+              <br />
+              <Input type="password" placeholder="Digite sua senha" />
+            </div>
+          </form>
+          <div className="mt-5">
+            <button className="w-60 h-14 rounded-4xl cursor-pointer text-branco bg-azul hover:bg-blue-600">
+              <strong>ACESSAR</strong>
+            </button>
+          </div>
+          <hr className="w-60 mt-5 mb-5 border-azul" />
+          <div className="">
+            <p>
+              É um aluno(a)?{" "}
+              <NavLink to="/loginStudent">
+                <span className="text-azul underline">Clique aqui.</span>
+              </NavLink>
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
