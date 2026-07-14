369a96c00a0970f4c25a40d8e11ef5547f23de2a
Gleison <gleisonrgomes@gmail.com>
2026-07-14 19:33:27 -0300

    feat: Value and OnChange added on input component.

--- src/components/Input/input.jsx
+++ src/components/Input/input.jsx
@@ -1,7 +1,13 @@
-export default function Input({ type, placeholder }) {
+export default function Input({ type, placeholder, value, onChange }) {
   return (
     <>
-      <input className="w-60 h-10 p-2 rounded-xl outline-none border-2 border-azul" type={type} placeholder={placeholder} />
+      <input
+        className="w-60 h-10 p-2 rounded-xl outline-none border-2 border-azul"
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
     </>
   );
 }
