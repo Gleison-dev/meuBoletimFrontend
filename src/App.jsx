a303c8b24e2246b7394afe00fff803f1c58151f0
Gleison <gleisonrgomes@gmail.com>
2026-07-16 10:40:17 -0300

    feat: Link to dashboard of student crearted.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home/home";
 import Header from "./components/Header/header";
 import LoginStudent from "./pages/LoginStudent/loginStudent";
 import LoginTeacher from "./pages/LoginTeacher/loginTeacher";
+import DashboardStudent from "./pages/DashboardStudent/dashboardStudent";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/loginStudent" element={<LoginStudent />}></Route>
         <Route path="/loginTeacher" element={<LoginTeacher />}></Route>
+        <Route path="/dashboardStudent" element={<DashboardStudent />}></Route>
       </Routes>
     </>
   );
