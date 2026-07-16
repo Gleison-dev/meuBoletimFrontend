9a47fe5eae5ea473a34a07391358129e813ad21a
Gleison <gleisonrgomes@gmail.com>
2026-07-16 10:10:35 -0300

    rename

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState } from "react";
+import { api } from "../services/api";
+
+export const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [token, setToken] = useState(localStorage.getItem("token") || null);
+
+  async function login(email, password) {
+    try {
+      const response = await api.post("/login", {
+        email,
+        password,
+      });
+
+      const token = response.data.token;
+
+      localStorage.setItem("token", token);
+
+      setToken(token);
+
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async function logout() {
+    localStorage.removeItem("token");
+    setToken(null);
+  }
+  return (
+    <AuthContext.Provider value={{ login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
