d4587e44ef2e23719c792b9de0a8939abbe16a0c
Gleison <gleisonrgomes@gmail.com>
2026-07-19 11:58:38 -0300

    fix: Expired token adjusted

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,13 +1,19 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Navigate, Outlet } from "react-router-dom";
 import isTokenExpired from "../utils/tokenUtils";
 
 export function PrivateRoute() {
   const { token, logout } = useContext(AuthContext);
+  const expired = !token || isTokenExpired(token);
 
-  if (!token || isTokenExpired(token)) {
-    logout();
+  useEffect(() => {
+    if (expired) {
+      logout();
+    }
+  }, [expired]);
+
+  if (expired) {
     return <Navigate to="/loginStudent" replace />;
   }
 
