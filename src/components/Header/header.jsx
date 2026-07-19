e4968ce4621ffe1e076fe9f8c90c89085f7b950d
Gleison <gleisonrgomes@gmail.com>
2026-07-19 12:38:46 -0300

    feat: Authentication on header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,25 +1,46 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import icon_user from "../../assets/icon_user.svg";
 
 export default function Header() {
+  const { token } = useContext(AuthContext);
   return (
     <>
       <section className="h-25 flex justify-between items-center shadow px-10 bg-azul-claro">
         <div>
-          <NavLink to="/">
+          {token ? (
             <h1 className="text-xl">
               <span className="font-semibold">Meu</span> <br />{" "}
               <span className="text-azul">
                 <strong>Boletim</strong>
               </span>
             </h1>
-          </NavLink>
+          ) : (
+            <NavLink to="/">
+              <h1 className="text-xl">
+                <span className="font-semibold">Meu</span> <br />{" "}
+                <span className="text-azul">
+                  <strong>Boletim</strong>
+                </span>
+              </h1>
+            </NavLink>
+          )}
         </div>
         <div>
-          <NavLink to="/loginStudent">
-            <button className="w-30 h-10 rounded-lg text-lg cursor-pointer bg-azul text-branco hover:bg-blue-600">
-              Entrar
-            </button>
-          </NavLink>
+          {token ? (
+            <img
+              className="w-15 h-15 cursor-pointer"
+              src={icon_user}
+              alt="Ícone de usuário"
+            />
+          ) : (
+            <NavLink to="/loginStudent">
+              <button className="w-30 h-10 rounded-lg text-lg cursor-pointer bg-azul text-branco hover:bg-blue-600">
+                Entrar
+              </button>
+            </NavLink>
+          )}
         </div>
       </section>
     </>
