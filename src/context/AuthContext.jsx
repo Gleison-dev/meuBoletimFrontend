164de475cf73c7934445dfa3dde56afa4e714808
Gleison <gleisonrgomes@gmail.com>
2026-07-21 14:50:22 -0300

    feat: Profile function created.

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,7 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { isTokenExpired } from "../utils/tokenUtils";
 import { api } from "../services/api";
-import { Navigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 
 export const AuthContext = createContext();
@@ -16,6 +15,7 @@ function getValidToken() {
 }
 
 export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
   const [token, setToken] = useState(localStorage.getItem("token") || null);
   const role = token ? jwtDecode(token).role : null;
 
@@ -43,8 +43,28 @@ export function AuthProvider({ children }) {
     localStorage.removeItem("token");
     setToken(null);
   }
+
+  async function profile() {
+    try {
+      const response = await api.get("/me", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setUser(response.data.user);
+    } catch (error) {
+      const message =
+        error.response?.data.message || "Erro ao buscar o perfil!";
+      return message;
+    }
+  }
+
+  useEffect(() => {
+    profile();
+  }, [token]);
+
   return (
-    <AuthContext.Provider value={{ token, login, role, logout }}>
+    <AuthContext.Provider value={{ token, login, role, profile, logout }}>
       {children}
     </AuthContext.Provider>
   );
