9e9fe9914429c0b7004e91df22403cb417dc91b9
Gleison <gleisonrgomes@gmail.com>
2026-08-02 21:03:31 -0300

    feat: Input component with a new sityle

--- src/components/Input/input.jsx
+++ src/components/Input/input.jsx
@@ -2,7 +2,7 @@ export default function Input({ type, placeholder, value, onChange }) {
   return (
     <>
       <input
-        className="w-60 h-10 p-2 rounded-xl outline-none border-2 border-azul"
+        className="w-60 h-10 p-2 outline-none bg-branco border-2 border-azul rounded-lg focus:border-blue-800"
         type={type}
         value={value}
         onChange={onChange}
