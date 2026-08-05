b3bed8dacac7afe74b61926228ba9136c2c3887e
Gleison <gleisonrgomes@gmail.com>
2026-08-05 20:58:41 -0300

    feat: Reset password page created.

--- src/pages/ResetPassword/resetPassword.jsx
+++ src/pages/ResetPassword/resetPassword.jsx
@@ -1,7 +1,26 @@
+import Input from "@/components/Input/input";
+
 export default function ResetPassword() {
   return (
     <>
-      <section></section>
+      <section className="flex justify-center items-center mt-10">
+        <div className="w-96 p-6 rounded-xl bg-azul-claro">
+          <div>
+            <h1 className="text-3xl">Esqueceu a senha?</h1>
+            <p>Sem problemas, a gente te ajuda!</p>
+          </div>
+          <div className="mt-4">
+            <label>
+              <strong>E-mail</strong>
+            </label>{" "}
+            <br />
+            <Input type="text" placeholder="Insira o e-mail cadastrado" />
+          </div>
+          <div className="flex justify-center items-center mt-5">
+            <button className="w-96 h-14 rounded-4xl text-xl cursor-pointer transition ease-in-out duration-300 text-white bg-azul hover:bg-blue-800">Verificar E-mail</button>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
