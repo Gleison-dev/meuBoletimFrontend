c628ce7fd848c7e98001085863e282a1016da734
Gleison <gleisonrgomes@gmail.com>
2026-08-16 19:44:03 -0300

    feat: Schema and styles of create class page

--- src/pages/CreateClass/createClass.jsx
+++ src/pages/CreateClass/createClass.jsx
@@ -1,12 +1,25 @@
+import Input from "@/components/Input/input";
+import Label from "@/components/Label/label";
+
 export default function CreateClass() {
   return (
     <>
-      <section>
+      <section className="p-4 rounded-lg bg-branco">
         <div>
-          <div>
-            <h1>Criar nova turma</h1>
-          </div>
+          <h1 className="text-xl">
+            <strong>Criar nova turma</strong>
+          </h1>
+          <p>Crie uma nova turma abaixo.</p>
         </div>
+        <form className="flex flex-col gap-4 mt-5">
+          <div>
+            <Label name="Nome da turma" /> <br />
+            <Input type="text" placeholder="Digite o nome da turma" />
+          </div>
+          <div>
+            <button className="w-80 h-12 rounded-md text-xl text-branco bg-azul">Criar turma</button>
+          </div>
+        </form>
       </section>
     </>
   );
