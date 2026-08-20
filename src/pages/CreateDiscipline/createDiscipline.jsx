38508974f4b82e43d5e287fe0f89385d02874406
Gleison <gleisonrgomes@gmail.com>
2026-08-20 19:09:31 -0300

    feat: Create discipline function

--- src/pages/CreateDiscipline/createDiscipline.jsx
+++ src/pages/CreateDiscipline/createDiscipline.jsx
@@ -1,7 +1,45 @@
 import Input from "@/components/Input/input";
 import Label from "@/components/Label/label";
+import { AuthContext } from "@/context/AuthContext";
+import { api } from "@/services/api";
+import { useContext, useState } from "react";
 
 export default function CreateDiscipline() {
+  const { token } = useContext(AuthContext);
+  const [name, setName] = useState("");
+  const [status, setStatus] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const response = await api.post(
+        "/createDiscipline",
+        {
+          name,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      setStatus(response.data.discipline);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      const message =
+        error.response?.data?.message || "Erro ao cadastrar disciplina.";
+      setStatus(message);
+    }
+    setTimeout(() => {
+      setName("");
+      setStatus("");
+    }, 3000);
+  };
+
   return (
     <>
       <section className="p-4 rounded-lg bg-branco">
@@ -12,13 +50,38 @@ export default function CreateDiscipline() {
             </h1>
             <p>Cadastre uma disciplina abaixo.</p>
           </div>
-          <form className="flex flex-col gap-3">
+          <form onSubmit={handleSubmit} className="flex flex-col gap-3">
             <div>
               <Label name="Nome da disciplina" /> <br />
-              <Input type="text" placeholder="Digite o nome da disciplina" />
+              <Input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Digite o nome da disciplina"
+              />
+            </div>
+            <div>
+              <p className="text-azul">
+                <strong>{status}</strong>
+              </p>
             </div>
             <div className="flex justify-center">
-              <button className="w-80 h-12 rounded-md text-xl text-branco bg-azul hover:bg-blue-800">Cadastrar</button>
+              {loading ? (
+                <button
+                  disabled
+                  type="submit"
+                  className="w-80 h-12 rounded-md text-xl text-branco bg-gray-500"
+                >
+                  Cadastrando...
+                </button>
+              ) : (
+                <button
+                  type="submit"
+                  className="w-80 h-12 rounded-md text-xl text-branco bg-azul hover:bg-blue-800"
+                >
+                  Cadastrar
+                </button>
+              )}
             </div>
           </form>
         </div>
