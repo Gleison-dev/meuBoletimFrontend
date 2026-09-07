c19ae36fd6f77862845d20b006f67fce5949b1b1
Gleison <gleisonrgomes@gmail.com>
2026-09-07 18:37:34 -0300

    feat: Link to notes of student.

--- src/components/CardStudent/cardStudent.jsx
+++ src/components/CardStudent/cardStudent.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import { DialogDemo } from "../ui/dialogDemo";
 
 export default function CardStudent({ id, name, disciplineId }) {
@@ -10,7 +11,12 @@ export default function CardStudent({ id, name, disciplineId }) {
           </h1>
         </div>
         <hr className="w-20 border border-azul" />
-        <div>
+        <div className="flex flex-col justify-center items-centers gap-2">
+          <NavLink to={`/noteOfStudent/${id}`}>
+            <button className="p-1 w-25 h-8 rounded-lg border border-gray-300 cursor-pointer hover:bg-azul-claro">
+              Ver nota
+            </button>
+          </NavLink>
           <DialogDemo studentId={id} disciplineId={disciplineId} />
         </div>
       </section>
