b8f39099e96f6eee139da800c4fd1195fe8214c5
Gleison <gleisonrgomes@gmail.com>
2026-09-10 20:40:22 -0300

    feat: Table notes

---
+++ src/components/ui/tableNotes.jsx
@@ -0,0 +1,28 @@
+import { MoreHorizontalIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export function TableNotes({ discipline, unit, note }) {
+  return (
+    <TableRow>
+      <TableCell className="font-medium">{discipline}</TableCell>
+      <TableCell className="text-center">{unit}ª</TableCell>
+      <TableCell className="text-center">{note}</TableCell>
+    </TableRow>
+  );
+}
