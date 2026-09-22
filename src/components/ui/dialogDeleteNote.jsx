b751c38dd6a4b5a78702a48435f2d0f61a677777
Gleison <gleisonrgomes@gmail.com>
2026-09-22 18:38:08 -0300

    feat: Type password on input.

--- src/components/ui/dialogDeleteNote.jsx
+++ src/components/ui/dialogDeleteNote.jsx
@@ -78,6 +78,7 @@ export function DialogDeleteNote({ id, studentId, onDeleteUpdate }) {
             <Field>
               <Label>Senha</Label>
               <Input
+                type={"password"}
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="Insira sua senha"
